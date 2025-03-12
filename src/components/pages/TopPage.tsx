0b0e08ada558278681979631c1c4db376552e835
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-03-12 10:37:57 +0900

    トップページのヘッター調整

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,17 +1,17 @@
 
 export default function TopPage() {
   return (
-    <div className="mt-5">
-        <header className="flex justify-between">
-            <div>
-                <p>スケジュール管理</p>
-            </div>
+    <div className="relative">
+        <header className="leading-[50px] fixed top-0 left-0 right-0">
+            <div className= "container mx-auto flex justify-between">
+                <p className="logo">スケジュール管理</p>
                 <nav>
-                    <ul className="flex gap-5">
+                    <ul className="flex gap-5 text-lime-800">
                         <li>利用説明</li>
                         <li>ログイン</li>
                     </ul>
                 </nav>
+            </div>
         </header>
     </div>
     );
