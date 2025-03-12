ba8fcd5158a14d026c38a6364adb1f99b4749a5f
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-03-12 11:04:18 +0900

    トップページのヘッター作成

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -1,16 +1,16 @@
 export default function TopPage() {
     return (
         <div>
-            <header className="flex justify-between mt-5">
-                <div>
-                    <p>スケジュール管理</p>
+            <header>
+                <div className="container mx-auto flex justify-between p-4">
+                    <p className="logo">スケジュール管理</p>
+                    <nav>
+                        <ul className="flex gap-5 text-lime-800">
+                            <li>利用説明</li>
+                            <li>ログイン</li>
+                        </ul>
+                    </nav>
                 </div>
-                <nav>
-                    <ul className="flex gap-5">
-                        <li>利用説明</li>
-                        <li>ログイン</li>
-                    </ul>
-                </nav>
             </header>
         </div>
     )
