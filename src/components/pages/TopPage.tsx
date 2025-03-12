63b0e2faf8389b40c5375876f0d0104158b79269
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-03-12 12:11:30 +0900

    トップページ作成

--- src/components/pages/TopPage.tsx
+++ src/components/pages/TopPage.tsx
@@ -2,7 +2,7 @@
 export default function TopPage() {
   return (
     <div className="relative">
-        <header className="leading-[50px] fixed top-0 left-0 right-0">
+        <header className="bg-white leading-[50px] fixed top-0 left-0 right-0">
             <div className= "container mx-auto flex justify-between">
                 <p className="logo">スケジュール管理</p>
                 <nav>
@@ -13,6 +13,19 @@ export default function TopPage() {
                 </nav>
             </div>
         </header>
+        <main className="pt-[50px] h-screen bg-gradient-to-r from-lime-100 to-lime-200 flex flex-col justify-center items-center">
+            <div className="flex flex-col items-center justify-center">
+                <h1 className="logo text-7xl">スケジュール管理APP</h1>
+                <p className="text-5xl pt-[10vh]">
+                    スケジュールを管理します。
+                </p>
+                <div className="pt-[20vh]">
+                    <button className="bg-lime-800 text-white p-4 rounded-md">
+                        ログイン
+                    </button>
+                </div>
+            </div>
+        </main>
     </div>
     );
 };
