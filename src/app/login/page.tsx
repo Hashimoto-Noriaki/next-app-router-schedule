7574318db85e97656bc7fd23ada84c9d2d61bbb3
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-03-12 15:52:48 +0900

    /loginでログイン画面に遷移

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import  NotLoginLayout  from "../../components/templates/NotLoginLayout";
+
+export default function LoginPage() {
+    return (
+        <NotLoginLayout>
+            <h1>ログイン</h1>
+        </NotLoginLayout>
+    );
+}
